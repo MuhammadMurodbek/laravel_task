f079e362da3237d0a22a4115981d9436b921dafa
Murodbek Raximjonov <murodbekraximjonov@Murodbeks-Air.home>
2023-07-30 16:31:54 +0500

    user_and_roles

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=>'manager',
+            'role_id'=>1,
+            'email'=>'manage@manage.com',
+            'password'=>Hash::make('secret')
+        ]);
+    }
+}
