45a9a5f480a81619245ba9bedc6f2c33482ebd13
Murodbek Raximjonov <murodbekraximjonov@Murodbeks-MacBook-Air.local>
2023-08-04 15:54:51 +0500

    mailable

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -6,8 +6,10 @@ use App\Events\ClientVisit;
 use App\Events\PostCreated;
 use App\Http\Requests\ApplicationRequest;
 use App\Jobs\ApplicationPodcast;
+use App\Mail\MailSend;
 use App\Models\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 class ApplicationController extends Controller
@@ -30,6 +32,7 @@ class ApplicationController extends Controller
         PostCreated::dispatch($application);
         ClientVisit::dispatch($application);
         ApplicationPodcast::dispatch($application);
+        Mail::to($request->user())->later(now()->addMilliseconds(200),(new MailSend($application))->onQueue('mailable'));
         return redirect()->back();
     }
 }
