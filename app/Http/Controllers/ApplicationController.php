b3e382ead9e936e9e9fbd750fe3223300bccd2d8
Murodbek Raximjonov <murodbekraximjonov@Murodbeks-MacBook-Air.local>
2023-08-07 17:55:10 +0500

    news

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -9,6 +9,7 @@ use App\Jobs\ApplicationPodcast;
 use App\Mail\MailSend;
 use App\Models\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
@@ -16,6 +17,10 @@ class ApplicationController extends Controller
 {
     public function store(ApplicationRequest $request){
 
+        $post = Application::whereDate('created_at',  Carbon::today())->first();
+    
+        if ($post) return redirect()->back()->with('error',' You can create only 1 post in a day. Please try tomorrow!');
+        
         if($request->hasFile('file')){
             $name = $request->file('file')->getClientOriginalName();
             $path = $request->file('file')->storeAs(
@@ -29,10 +34,12 @@ class ApplicationController extends Controller
             'message'=>$request->message,
             'file_url'=>$path ?? null,
         ]);
+
         PostCreated::dispatch($application);
         ClientVisit::dispatch($application);
         ApplicationPodcast::dispatch($application);
         Mail::to($request->user())->later(now()->addMilliseconds(200),(new MailSend($application))->onQueue('mailable'));
+        
         return redirect()->back();
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -48,6 +48,35 @@
             </div>
         @else
             <div class="flex flex-col items-center justify-center px-12 py-5">
+                @if (session()->has('error'))
+                    <div
+                        class="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-md text-red-700 bg-red-100 border border-red-300 ">
+                        <div slot="avatar">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-alert-octagon w-5 h-5 mx-2">
+                                <polygon
+                                    points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2">
+                                </polygon>
+                                <line x1="12" y1="8" x2="12" y2="12"></line>
+                                <line x1="12" y1="16" x2="12.01" y2="16"></line>
+                            </svg>
+                        </div>
+                        <div class="text-xl font-normal  max-w-full flex-initial">
+                            {{ session()->get('error') }}</div>
+                        <div class="flex flex-auto flex-row-reverse">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="feather feather-x cursor-pointer hover:text-red-400 rounded-full w-5 h-5 ml-2">
+                                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                @endif
                 <p class="mb-4 font-semibold text-xl px-8 py-2 rounded-md">Submit your
                     application</p>
                 <div class="mx-auto w-full max-w-[550px]">
